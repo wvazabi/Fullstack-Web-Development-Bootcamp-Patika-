1cd8e5f44c2be86d0d909ea3ee6a9d0254a46c50
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-15 18:02:21 +0300

    While Practice

--- Week #1/Java Pratik/src/Main.java
+++ Week #1/Java Pratik/src/Main.java
@@ -27,13 +27,47 @@ public class Main {
 
         // iki boyutlu arraylerde işlemler
 
-        String[] name = {"Enes0","Enes2","Enes3","Enes4","Enes5"};
-        int index = 0;
+//        String[] name = {"Enes0","Enes2","Enes3","Enes4","Enes5"};
+//        int index = 0;
+//
+//        while( index < name.length ) {
+//            System.out.println(name[index]);
+//            index++;
+//        }
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("sayı giriniz");
+//
+//        int num = sc.nextInt();
+//        int sum = 0;
+//
+//
+//        while ( num > 0 ) {
+//            sum += (num % 10);
+//            num /= 10;
+//
+//        }
+//
+//        System.out.println(sum);
 
-        while( index < name.length ) {
-            System.out.println(name[index]);
+        int[] numbers = { 3,5 };
+        int index = 0;
+        int toplam = 0;
+        int lenght = numbers.length;
+        System.out.println(lenght);
+
+
+        while( index < lenght) {
+            System.out.println(numbers[index]);
+            toplam += numbers[index];
             index++;
         }
+
+        System.out.println("\n" + toplam);
+
+
+
+
 
 
     }
