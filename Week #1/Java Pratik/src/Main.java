08def166fbafd74b4af414962c288f430ab823be
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-15 15:44:48 +0300

    Add Arrays

---
+++ Week #1/Java Pratik/src/Main.java
@@ -0,0 +1,18 @@
+
+import  java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        String [] names = new String[5];
+
+        names[0] = "enes0";
+        names[1] = "enes1";
+        names[2] = "enes2";
+        names[3] = "enes3";
+        names[4] = "enes4";
+
+        System.out.println(Arrays.toString(names));
+
+
+    }
+}
+
