762767c16c7f6331f0b28210c53cc16735b921e6
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-15 16:26:19 +0300

    Add Split Method

--- Week #1/Java Pratik/src/Main.java
+++ Week #1/Java Pratik/src/Main.java
@@ -1,16 +1,30 @@
 
 import  java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String [] names = new String[5];
+//        String [] names = new String[5];
+//
+//        names[0] = "enes0";
+//        names[1] = "enes1";
+//        names[2] = "enes2";
+//        names[3] = "enes3";
+//        names[4] = "enes4";
+//
+//        System.out.println(Arrays.toString(names));
+// Java split metodu ile cümleyi bölme işlemi
 
-        names[0] = "enes0";
-        names[1] = "enes1";
-        names[2] = "enes2";
-        names[3] = "enes3";
-        names[4] = "enes4";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Your Sentence");
+        String sentence = sc.nextLine();
+        System.out.println(sentence);
 
-        System.out.println(Arrays.toString(names));
+        String [] words = sentence.split(" ");
+        System.out.println(words[0]);
+        System.out.println(words[1]);
+        System.out.println(words[2]);
+
 
 
     }
