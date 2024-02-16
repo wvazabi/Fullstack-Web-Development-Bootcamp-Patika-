2781ab6baf053b703db8adde7735212da01c694c
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-16 12:29:57 +0300

    Add Find Num Method

--- Week #1/Methods/src/Main.java
+++ Week #1/Methods/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 0;
+        int num2 = 12;
+        int[] arr = {3,5,6,12,7,4};
+
+        //sum(num1,num2);
+        find(num1,arr);
+
+
+
     }
+
+    public static void sum(int num1, int num2) {
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+
+    public static void find(int num1, int[] arr) {
+
+        boolean find = false;
+
+        for(int num : arr) {
+            if(num == num1) {
+                find = true;
+                break;
+            }
+        }
+
+        System.out.println("find : " + find);
+
+    }
+
+
 }
