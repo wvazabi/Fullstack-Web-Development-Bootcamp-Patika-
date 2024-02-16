a79e68a4a701b7a2f71780426723b9d2cd3147e8
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-16 17:02:38 +0300

    Add Faktoriel  Method

--- Week #1/Methods/src/Main.java
+++ Week #1/Methods/src/Main.java
@@ -1,16 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        int num1 = 0;
-        int num2 = 12;
+        int num1 = 3;
+        int num2 = 6;
         int[] arr = {3,5,6,12,7,4};
 
         //sum(num1,num2);
-        find(num1,arr);
+       // find(num1,arr);
+//       int sum =  sumR(num1, num2);
+//       System.out.println(sum);
 
-
+        int fact = factorial(num2);
+        System.out.println(fact);
 
     }
 
+
+    public static int factorial(int num) {
+        int result = 1;
+
+        for(int i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+    public static int sumR(int num1, int num2) {
+        int sum = num1 + num2;
+
+        return sum;
+    }
     public static void sum(int num1, int num2) {
         int sum = num1 + num2;
 
