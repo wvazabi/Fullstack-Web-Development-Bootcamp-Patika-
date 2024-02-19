c0d67f9dcba0afa1faf2b02411da8d41ea5221f7
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-20 00:39:13 +0300

    Update VAT

--- Week #2/Calculation VAT/src/Main.java
+++ Week #2/Calculation VAT/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args){
 
         //Variables are defined
-        double price,vat,priceExVAT;
+        double price,vat,priceExVAT,consVAT;
 
         //Scanner class was defined
         System.out.println("Please Enter a Price of Product");
@@ -11,10 +11,15 @@ public class Main {
 
         //The value obtained from user is assigned a variable
         price = input.nextDouble();
+        if (price >= 0 && price <= 1000) {
+            consVAT = 0.18;
+        } else {
+            consVAT = 0.08;
+        }
 
-        priceExVAT = price - ((price/100) * 18);
+        vat = (price * consVAT);
 
-        vat = ((price/100) * 18);
+        priceExVAT = price - vat;
 
 
         System.out.println(
