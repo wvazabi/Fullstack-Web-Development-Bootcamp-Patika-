e1e5f3a6b5a0ab18c9ff48b7a07f8abf757d8a85
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-20 00:26:13 +0300

    Update Function

--- Week #2/Calculation VAT/src/Main.java
+++ Week #2/Calculation VAT/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        //Variables are defined
+        int price,vat,priceExVAT;
+
+        //Scanner class was defined
+        System.out.println("Please Enter a Price of Product");
+        Scanner input = new Scanner(System.in);
+
+        //The value obtained from user is assigned a variable
+        price = input.nextInt();
+
+        priceExVAT = price - ((price/100) * 18);
+
+        vat = ((price/100) * 18);
+
+
+        System.out.println(
+                "Price Excluding VAT = " + priceExVAT +
+                "\nPrice Including VAT = " + price +
+                "\nValue Added TAX = " + vat);
+
+
     }
+
+
+
+
+
 }
