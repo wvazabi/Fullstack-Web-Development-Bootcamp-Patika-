022885f3f649c47109a4980efd9e0909eb976cbd
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-22 18:38:40 +0300

    Perfect Number

---
+++ Week #2/Perfect Number/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+    }
+}
