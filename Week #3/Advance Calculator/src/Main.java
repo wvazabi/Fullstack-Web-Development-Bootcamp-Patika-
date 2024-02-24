8b55161b1b4f6c382ebe8da6abbe5ce48fe77738
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-25 00:40:44 +0300

    Advance  Calculator

---
+++ Week #3/Advance Calculator/src/Main.java
@@ -0,0 +1,156 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void addition() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 0, i = 1;
+        while (true) {
+            System.out.print(i++ + ". number :");
+            number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            result += number;
+        }
+        System.out.println("Result : " + result);
+    }
+
+    static void subtraction() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers will you enter :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". number :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void multiplication() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, i = 1;
+
+        while (true) {
+            System.out.print(i++ + ". number :");
+            number = scan.nextInt();
+
+            if (number == 1)
+                break;
+
+            if (number == 0) {
+                result = 0;
+                break;
+            }
+            result *= number;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void division() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers will you enter :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". number :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("You cannot divide by 0.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the base value:");
+        int base = scan.nextInt();
+        System.out.print("Enter the exponent value:");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+    static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Result : " + result);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int select;
+        String menu = "1- Addition Operation\n"
+                + "2- Subtraction Operation\n"
+                + "3- Multiplication Operation\n"
+                + "4- Division Operation\n"
+                + "5- Exponential Calculation\n"
+                + "6- Factorial Calculation\n"
+                + "7- Modulus Operation\n"
+                + "8- Rectangle Area and Perimeter Calculation\n"
+                + "0- Exit";
+
+        do {
+            System.out.println(menu);
+            System.out.print("Please select an operation :");
+            select = scan.nextInt();
+            switch (select) {
+                case 1:
+                    addition();
+                    break;
+                case 2:
+                    subtraction();
+                    break;
+                case 3:
+                    multiplication();
+                    break;
+                case 4:
+                    division();
+                    break;
+                case 5:
+                    power();
+                    break;
+                case 6:
+                    factorial();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("You entered a wrong value, please try again.");
+            }
+        } while (select != 0);
+
+
+    }
+}
