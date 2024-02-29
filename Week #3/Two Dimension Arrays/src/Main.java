05bd8b7d01e2028ff0b6fc049df8407c406054f9
Enes Kaya <eng.eneskaya@gmail.com>
2024-03-01 01:41:43 +0300

    Two. dimensional array

---
+++ Week #3/Two Dimension Arrays/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Matris boyutunu sizin belirlediğiniz matris değerlerini ise kullanıcının girdiği bir algoritma yazınız.
+
+      Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter row value");
+//
+//        int row = input.nextInt();
+//        System.out.println("Enter column value");
+//        int col = input.nextInt();
+//
+
+        int [][] matris = new int[3][3];
+
+        for (int i = 0; i < matris.length; i++) {
+            for (int j = 0; j < matris[i].length; j++) {
+                System.out.println("Enter the value");
+                int x = input.nextInt();
+                matris[i][j] = x;
+            }
+        }
+
+        for (int i = 0; i < matris.length; i++) {
+            System.out.println();
+            for (int j = 0; j < matris[i].length; j++) {
+                System.out.print(matris[i][j] + " ");
+            }
+        }
+
+
+
+
+
+    }
+}
