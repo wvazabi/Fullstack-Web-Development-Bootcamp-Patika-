5154838ff6c8418d8439d45ac00bca9f17619647
Enes Kaya <eng.eneskaya@gmail.com>
2024-02-29 10:24:16 +0300

    Update Salary

--- Week #3/Maas Hesaplama/src/Employee.java
+++ Week #3/Maas Hesaplama/src/Employee.java
@@ -1,74 +1,73 @@
 public class Employee {
+
+    //Definition of attributes
     String name;
     double salary;
     int workHours;
     int hireYear;
 
+    //Creating the constructor class
     Employee(String name, double salary, int workHours, int hireYear) {
         this.name = name;
         this.salary = salary;
         this.workHours = workHours;
         this.hireYear = hireYear;
-
     }
 
-    double tax(double salary) {
-        if (salary <= 1000) {
+    //Method to calculate tax
+    double tax() {
+        if (this.salary <= 1000) {
             return 0;
+        } else {
+            return this.salary * 0.03;
         }
-        else {
-            return salary * 0.03;
-        }
-
     }
 
-    int bonus(int workHour){
-        if (workHour > 40) {
-            workHour =  (workHour - 40) * 30;
-            return workHour;
+    //Bonus calculation method
+    int bonus() {
+        if (this.workHours > 40) {
+            return (this.workHours - 40) * 30;
         }
-        else {
-            return 0;
-        }
-
+        return 0;
     }
 
+    //Method of calculating salary increase
     double raiseSalary(int hireYear, double salary) {
-       final int year = 2024;
-       int tempYear = year - hireYear;
-        if(tempYear < 10) {
+        final int year = 2024;
+        if (year - hireYear < 10) {
             return salary * 0.05;
-        } else if (tempYear > 9 && tempYear < 20) {
+        } else if ((year - hireYear) > 9 && (year - hireYear) < 20) {
             return salary * 0.10;
-        } else  {
+        } else {
             return salary * 0.15;
         }
-
     }
 
-    double newSalary(){
-        double tax = tax(this.salary);
-        int bonus = bonus(this.workHours);
-        double raise = raiseSalary(this.hireYear,this.salary);
-        return (raise - tax) + bonus + this.salary;
-    }
-
+    //Method of calculating salary with taxes and bonuses
     double bonusAndTax(){
-        return this.salary + this.bonus(this.workHours) - this.tax(this.salary);
+        return this.salary + this.bonus() - this.tax();
     }
 
-    void toString(Employee emp1) {
-        System.out.println("Adı:\t" + emp1.name);
-        System.out.println("Maaşı:\t" + emp1.salary);
-        System.out.println("Çalışma saati:\t" + emp1.workHours);
-        System.out.println("Başlangıç yılı:\t" + emp1.hireYear);
-        System.out.println("Vergi kesintisi:\t" + emp1.tax(emp1.salary));
-        System.out.println("Bonus:\t" + emp1.bonus(emp1.workHours));
-        System.out.println("Maaş artışı:\t" + emp1.raiseSalary(emp1.hireYear, emp1.salary));
-        System.out.println("Vergi ve bonus\t" + emp1.bonusAndTax());
-        System.out.println("Yeni Maaş:\t" + emp1.newSalary());
+    //New salary calculation method
+    double newSalary(){
+        double tax = tax();
+        int bonus = bonus();
+        double raiseSalary = raiseSalary(this.hireYear, this.salary);
+        return raiseSalary - tax + bonus + this.salary;
     }
 
+    //Printing the result to the screen
+    void toString(Employee employee){
+        System.out.println("Adı: " + employee.name);
+        System.out.println("Maaşı: " + employee.salary);
+        System.out.println("Çalışma Saati: " + employee.workHours);
+        System.out.println("Başlangıç Yılı: " + employee.hireYear);
+        System.out.println("Vergi Kesintisi: " + employee.tax());
+        System.out.println("Bonus: " + employee.bonus());
+        System.out.println("Maaş Artışı : " + employee.raiseSalary(employee.hireYear, employee.salary));
+        System.out.println("Vergi ve Bonuslar ile birlikte maaş : " + employee.bonusAndTax());
+        System.out.println("Toplam Maaş: " + employee.newSalary());
+    }
 
 
 }
