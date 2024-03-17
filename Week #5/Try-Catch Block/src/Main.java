0e82ddb93b5f7a816d816384d3c2f1114069023e
Enes Kaya <eng.eneskaya@gmail.com>
2024-03-17 14:16:09 +0300

    Try-Catch Block

---
+++ Week #5/Try-Catch Block/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] age = new int[10];
+
+        for (int i : age) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
