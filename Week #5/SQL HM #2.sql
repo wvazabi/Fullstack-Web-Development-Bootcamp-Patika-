ccf187d0f5a15a0adc4ddca035a76c1fa0619e02
Enes Kaya <eng.eneskaya@gmail.com>
2024-03-24 15:28:38 +0300

    SQL HM #2

---
+++ Week #5/SQL HM #2.sql
@@ -0,0 +1,13 @@
+SELECT * FROM film 
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+SELECT * FROM actor
+WHERE first_name IN ('Penelope', 'Nick', 'Ed');
+
+SELECT * FROM film
+WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
+
+
+
+
+
