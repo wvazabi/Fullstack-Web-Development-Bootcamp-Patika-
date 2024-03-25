29ecdcff59b8c03a72cb51b802d2851964da2b87
Enes Kaya <eng.eneskaya@gmail.com>
2024-03-25 23:45:33 +0300

    Update

--- Week #6/SQL H W#5.sql
+++ Week #6/SQL H W#5.sql
@@ -1,1 +1,19 @@
-SELECT * FROM country;
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length 
+OFFSET 5
+LIMIT 5;
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
+
+
+
+
