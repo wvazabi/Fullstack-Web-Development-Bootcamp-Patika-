10f05d0edf2772677fdb6a3f3830c807c7040b38
Enes Kaya <eng.eneskaya@gmail.com>
2024-04-01 00:33:57 +0300

    DB Statement

---
+++ Week #6/JavaDatabase/src/DBConnect.java
@@ -0,0 +1,69 @@
+import java.sql.*;
+
+public class DBConnect {
+    public static final String DB_URL = "jdbc:mysql://localhost/university";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "mysql";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement st = null;
+        try {
+            // TODO kayıt getirme
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            st = conn.createStatement();
+//            ResultSet resultSet = st.executeQuery("SELECT * FROM student");
+//            while (resultSet.next()) {
+//                System.out.println("ID : " + resultSet.getInt("student_id"));
+//                System.out.println("Adı : " + resultSet.getString("student_name"));
+//                System.out.println("Sınıfı : " + resultSet.getInt("student_class"));
+//                System.out.println("#################");
+//            }
+            // TODO kayıt eklemek
+//            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+//            st = conn.createStatement();
+//
+//            // Statement ile Insert İşlemi
+//            String updateQuery = "INSERT INTO student (student_name,student_class) VALUES ('Barış', 1)";
+//            st.executeUpdate(updateQuery);
+//
+//            // PreparedStatement ile Insert İşlemi
+//            PreparedStatement pr = conn.prepareStatement("INSERT INTO student (student_name,student_class) VALUES (?,?)");
+//            pr.setString(1, "Harry");
+//            pr.setString(2, "2");
+//            pr.executeUpdate();
+
+            // TODO Kayıt Güncelleme
+
+            // Statement ile Update İşlemi
+//            String updateQuery = "UPDATE student SET student_class = '1'  WHERE student_id = 3";
+//            st.executeUpdate(updateQuery);
+//
+//            // PreparedStatement ile Update İşlemi
+//            PreparedStatement pr = conn.prepareStatement("UPDATE student SET student_class = ?  WHERE student_id = ?");
+//            pr.setString(1, "6");
+//            pr.setInt(2, 2);
+//            pr.executeUpdate();
+
+            // TODO kayıt silme
+
+            // Statement ile Delete İşlemi
+            String updateQuery = "DELETE FROM student WHERE student_id = 4";
+            st.executeUpdate(updateQuery);
+
+            // PreparedStatement ile Delete İşlemi
+            PreparedStatement pr = conn.prepareStatement("DELETE FROM student WHERE student_id = ?");
+            pr.setInt(1, 4);
+            pr.executeUpdate();
+
+            st.close();
+
+        } catch (SQLException ex) {
+
+                System.out.println("SQLException: " + ex.getMessage());
+                System.out.println("SQLState: " + ex.getSQLState());
+                System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+}
