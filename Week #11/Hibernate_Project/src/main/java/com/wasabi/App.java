b28cd35740ab8a13ddd829f6044ebbf07c187343
Enes Kaya <eng.eneskaya@gmail.com>
2024-05-05 22:29:10 +0300

    Hibernate config

--- Week #11/Hibernate_Project/src/main/java/com/wasabi/App.java
+++ Week #11/Hibernate_Project/src/main/java/com/wasabi/App.java
@@ -1,7 +1,33 @@
 package com.wasabi;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Hibernate'de JPA yi sessionlar üzerinden yönetiyoruz JPA'da
+        // entity managerdan yönetiyorduk
+
+        // Hibernate Configuration
+        // Persistence xml yerine
+        SessionFactory factory = new Configuration()
+                .
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        // Build Hibernate Session Factory
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        // Open a session
+        try (Session session = sessionFactory.openSession()) {
+            // Hibernate operations go here
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
     }
 }
