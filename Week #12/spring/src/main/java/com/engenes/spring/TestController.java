287b6ae48b6f67841bfacb204e5b214b15ae7bed
Enes Kaya <eng.eneskaya@gmail.com>
2024-05-09 15:02:52 +0300

    Controller

---
+++ Week #12/spring/src/main/java/com/engenes/spring/TestController.java
@@ -0,0 +1,11 @@
+package com.engenes.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//Tarayıcıda bu dosyadan işlem yapabilirim demek
+@Controller
+@ResponseBody
+public class TestController {
+
+}
