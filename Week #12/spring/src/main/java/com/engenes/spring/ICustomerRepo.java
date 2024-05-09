c898ffbc3aeb9f4cb803929625992edf8bc66589
Enes Kaya <eng.eneskaya@gmail.com>
2024-05-10 00:22:06 +0300

    Custom Query

--- Week #12/spring/src/main/java/com/engenes/spring/ICustomerRepo.java
+++ Week #12/spring/src/main/java/com/engenes/spring/ICustomerRepo.java
@@ -1,12 +1,21 @@
 package com.engenes.spring;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 //CRUD Repository, JPA Repository
 @Repository
 public interface ICustomerRepo extends JpaRepository<Customer,Integer> {
 
     Customer findBycustomerMail (String mail);
+
+    //Customer query
+    @Query("FROM Customer as c WHERE c.customerGender = :gender")
+     List<Customer> queryByGender(@Param("gender") Customer.GENDER gender);
+
 
 }
