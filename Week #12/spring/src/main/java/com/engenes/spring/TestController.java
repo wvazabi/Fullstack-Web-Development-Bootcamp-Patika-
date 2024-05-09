a6b297c505815ef994d21d988be95a4b967bea9d
Enes Kaya <eng.eneskaya@gmail.com>
2024-05-09 16:18:16 +0300

    Get Mapping

--- Week #12/spring/src/main/java/com/engenes/spring/TestController.java
+++ Week #12/spring/src/main/java/com/engenes/spring/TestController.java
@@ -1,11 +1,22 @@
 package com.engenes.spring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //Tarayıcıda bu dosyadan işlem yapabilirim demek
 @Controller
 @ResponseBody
+// browser'a ne yazdığında çalıştırcağını söylüyoruz
+@RequestMapping("/")
 public class TestController {
+
+    //url adresi mappleme işlemi
+    @GetMapping("/")
+    public String home(){
+        return "Anasayfa";
+    }
+
 
 }
