b0152b32c1c57799b47b97430d5caf195aea8120
Enes Kaya <eng.eneskaya@gmail.com>
2024-05-09 16:47:27 +0300

    Rest Controller

--- Week #12/spring/src/main/java/com/engenes/spring/TestController.java
+++ Week #12/spring/src/main/java/com/engenes/spring/TestController.java
@@ -1,14 +1,13 @@
 package com.engenes.spring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 //Tarayıcıda bu dosyadan işlem yapabilirim demek
 @Controller
 @ResponseBody
 // browser'a ne yazdığında çalıştırcağını söylüyoruz
+// ortak olan pathi burda yazabiliriz mesal tr
 @RequestMapping("/")
 public class TestController {
 
@@ -17,6 +16,18 @@ public class TestController {
     public String home(){
         return "Anasayfa";
     }
+
+    @GetMapping("/about")
+    //@RequestMapping(value = "/about", method = RequestMethod.GET) buda aynı işlevi görüyor
+    public String about(){
+        return "About Us Page";
+    }
+    // tarayıcı üzerinden urlden veri almak
+    @GetMapping("/user/{userId}/{userName}")
+    public String getUser(@PathVariable("userId") int user_id, @PathVariable("userName") String userName ) {
+        return "User id : " + user_id + " " + userName;
+    }
+
 
 
 }
