db9bf5d98011ac2082d376769bca80844d3fcb4e
Enes Kaya <eng.eneskaya@gmail.com>
2024-08-21 19:29:34 +0300

    Add Js

---
+++ FE Week#10/index.js
@@ -0,0 +1,45 @@
+// console.log("Merhaba Dünya");
+const personName = "Ahmet";
+// let greetingTemplate = `Merhaba ${personName}!`;
+// console.log(greetingTemplate);
+
+//  personName = "Mehmet";
+// console.log(personName);
+
+// {
+//     let personName = "Mehmet";
+//     {
+//         personName = "Ali";
+//         let greetingTemplate = `Merhaba ${personName}!`;
+//         {
+
+//         }
+//     }
+//     greetingTemplate = `Merhaba ${personName}!`;
+
+// }
+
+// const $ = (function jQuery() {
+//     var size = 800;
+//     function ajax() {
+//         console.log("Ajax işlemi yapılıyor...");
+//     }
+
+//     return {
+//         size: size,
+//         ajax: ajax
+//     }
+// })();
+
+// // modul gibi davranınca uygulamalar kendi modullerini geliştirdi
+// $.ajax();
+// console.log($.size);
+
+const personName2 = "Enes";
+
+// console.log(typeof personName2);
+
+console.log(typeof null);
+
+
+
